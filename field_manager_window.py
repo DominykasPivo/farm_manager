5550cd2ea8e5bf3f648cae9fb5ddd0bf633027ba
Dom <domas.pivoras@gmail.com>
2025-08-19 17:36:19 +0300

    Squashed commit of the following:
    commit c88de8f639ecc778de149f4018e6aacd979233f8
    Author: Dom <domas.pivoras@gmail.com>
    Date:   Tue Aug 19 17:18:46 2025 +0300
    
        field log functionality added
    
        fixed some issues with field view widget and added logs
    
    commit b7669bca8ead1a2d74671bc7b82021156bda0db7
    Author: Dom <domas.pivoras@gmail.com>
    Date:   Sat Aug 16 22:53:42 2025 +0300
    
        few tweaks
    
        need to finish go_back button
    
    commit 3a20d2566fba8c751a68165f5b34ea197b4a4f21
    Author: Dom <domas.pivoras@gmail.com>
    Date:   Tue Aug 12 14:18:20 2025 +0300
    
        functionality tweaks
    
        add field + view field functionality
    
    commit 061cb39127e8ca829ed1b72742cddddf815fd486
    Author: Dom <domas.pivoras@gmail.com>
    Date:   Mon Aug 11 23:39:53 2025 +0300
    
        start
    
        getting to know PyQt6

---
+++ field_manager_window.py
@@ -0,0 +1,55 @@
+from PyQt6.QtWidgets import QWidget, QLabel, QPushButton, QComboBox
+from field_manager import AddFieldWindow, ViewFieldWindow   
+import os, shutil
+
+# second window for the entire application (practically the main window)
+class Field_Manager(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        #GUI
+        self.setWindowTitle('Field Manager')
+        self.setGeometry(100, 100, 300, 300)
+        label = QLabel('This is the field manager window!', self)
+        label.move(60, 80)
+
+        #add btn to field
+        add_field_btn = QPushButton('Prideti Lauka', self)
+        add_field_btn.move(100, 120)
+        add_field_btn.clicked.connect(self.open_add_field_window)
+
+        self.all_fields_box = QComboBox(self)
+        self.all_fields_box.move(100, 210)
+        self.refresh_fields()
+
+        check_field_btn = QPushButton('Patikrinti Lauka', self)
+        check_field_btn.move(100, 240)
+        check_field_btn.clicked.connect(self.view_selected_field)
+
+        self.add_field_window = None
+        self.view_field_window = None
+
+    def refresh_fields(self):
+        fields_dir = 'fields'
+        field_names = []
+        if os.path.exists(fields_dir) and len(os.listdir(fields_dir)) > 0:
+            field_names = [name for name in os.listdir(fields_dir)
+                      if os.path.isdir(os.path.join(fields_dir, name))]
+        else:
+            field_names = ["None"]
+        self.all_fields_box.clear()
+        self.all_fields_box.addItems(field_names)
+
+    def open_add_field_window(self):
+        add_window = AddFieldWindow()
+        add_window.exec()  
+        self.refresh_fields()  
+    
+    def view_selected_field(self):
+        field_name = self.all_fields_box.currentText()
+        if field_name and field_name != "None":
+            self.view_field_window = ViewFieldWindow(field_name, parent=self, main_window=self)
+            self.view_field_window.show()
+            #self.hide()
+            
+
